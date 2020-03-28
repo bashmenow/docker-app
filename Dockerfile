bb6154b8cca2e368d393adef72220914940f71df
bashmenow <bashmenow@gmail.com>
2020-03-28 21:50:26 +0300

    edit requirements

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,15 @@
 FROM python:3.8
+
+WORKDIR /app
+COPY app /app
+COPY requirements.txt /app/
+COPY entry-point.sh /
+
 RUN groupadd -r user \
         && useradd -r -g user user \
         && pip install -r requirements.txt
-WORKDIR /app
-COPY app /app
-COPY entry-point.sh /
+
+
 EXPOSE 9090 9191
 USER user
 CMD ["/entry-point.sh"]
--- app/tests.py
+++ app/tests.py
@@ -6,13 +6,12 @@ class TestCase(unittest.TestCase):
         app.app.config["TESTING"] = True
         self.app = app.app.test_client()
     def get_main(self):
-        page = self.app.post("/", data=dict(name="Moby Dick"))
- assert page.status_code == 200
- assert 'Hello' in str( page.data )
- assert 'Moby Dick' in str( page.data )
- def test_html_escaping( self ):
- page = self.app.post( "/", data=dict(name='"><b>TEST</b><!--') )
- assert '<b>' not in str( page.data )
+        page = self.app.post("/", data=dict(domain="mail.emag.ru"))
+        assert page.status_code == 200
+        assert 'check' in str(page.data)
+        assert 'ptr' in str(page.data)
+    def test_html_escaping( self ):
+        page = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
+        assert '<b>' not in str(page.data)
 if __name__ == '__main__':
- unittest.main()
-Это очень простой тесто
+    unittest.main()
