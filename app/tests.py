a014c3afa8a4453f7bc08ecb7cae75462f33bd54
kravets <kravets@mskPC.msk.loc>
2020-03-29 12:42:03 +0300

    add uwsgi

--- app/tests.py
+++ app/tests.py
@@ -1,16 +1,16 @@
 import unittest
-import app
+import main
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        app.app.config["TESTING"] = True
-        self.app = app.app.test_client()
-    def get_main(self):
+        main.app.config["TESTING"] = True
+        self.app = main.app.test_client()
+    def test_get_mainpage(self):
         page = self.app.post("/", data=dict(domain="mail.emag.ru"))
         assert page.status_code == 200
-        assert 'check' in str(page.data)
-        assert 'ptr' in str(page.data)
-    def test_html_escaping( self ):
+        assert 'steu' in str(page.data)
+        assert 'PTR' in str(page.data)
+    def test_html_escaping(self):
         page = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
         assert '<b>' not in str(page.data)
 if __name__ == '__main__':
