66a12b3c6ab371a9f191d8eb9f4d36257bf42b34
bashmenow <bashmenow@gmail.com>
2020-04-14 21:59:50 +0300

    add proxy 2

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ RUN groupadd -r user \
         && useradd -r -g user user \
         && pip install -r requirements.txt
 
-
 EXPOSE 9090
 USER user
 CMD ["/entry-point.sh"]
--- entry-point.sh
+++ entry-point.sh
@@ -8,5 +8,5 @@ elif [ "$ENV" = 'UNIT' ]; then
  exec python "tests.py"
 elif [ "$ENV" = 'PROD' ]; then
  echo "Running Production Server"
- exec gunicorn --bind 0.0.0.0:9090 main.py:app
+ exec gunicorn --bind 0.0.0.0:9090 main:app
 fi
